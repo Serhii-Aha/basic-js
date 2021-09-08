87b128732a58f83e70ba57d03020daa4990ee739
unknown <a1002android@gmail.com>
2021-09-08 17:53:28 +0300

    feat: dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray (members)) {
+    return false;
+  }
+  let tempArr = [];
+  let tempElem;
+  for (let i = 0; i < members.length; i++) {
+    // если строка, то обрабатываем
+    if (typeof members[i] == 'string') {
+      // пробелы, верхний регистр, оставляем первый элемент
+      tempElem = members[i].trim ().toUpperCase ().substring (0, 1);
+      // добавляем в новый массив
+      tempArr.push (tempElem);
+    }
+  }
+  // let s = tempArr.sort().join('');
+  // сортируем и переводим в строку
+  return tempArr.sort ().join ('');
 }
