698ebbf36db29692bb2fb5f62dea3d8ec5d694c7
unknown <a1002android@gmail.com>
2021-09-08 17:49:31 +0300

    fix count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
+export default function countCats(matrix) {
   let col = 0;
   // "выравниваем" двумерный массив
   matrix
