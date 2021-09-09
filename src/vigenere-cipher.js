29c516b976d42ec565a51bff4acf6da2049a66c9
unknown <a1002android@gmail.com>
2021-09-09 20:56:59 +0300

    refactor: ?

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,69 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (mode){
+    this.mode = String(mode);
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getStringFromKey(str,key){
+    let strToUpp = str.toUpperCase()
+    let keyLength = key.length;
+    let keyToStr = ''
+    let k = 0;
+    for (let i = 0; ; i++) {
+      if (k === keyLength) {
+        k = 0;
+      }
+  
+      if (keyToStr.length === strToUpp.length) {
+        break
+      }
+      if(!this.alphabet.includes(strToUpp[i])){
+        keyToStr += strToUpp.charAt(i)
+      } else {
+      keyToStr += key.charAt(k);
+      k++
+      }
+    }
+    return keyToStr.toUpperCase();
+  }
+  encrypt(str, key) {
+    let strToUpp = str.toUpperCase()
+    let keyToStr = this.getStringFromKey(str, key)
+    //
+    let cipherText = ""
+    for (let j = 0; j < str.length; j++){
+      if(this.alphabet.includes(strToUpp[j])){
+        let x = ((strToUpp.charCodeAt(j) - 65) + (keyToStr.charCodeAt(j) - 65)) % 26;
+        cipherText += String.fromCharCode(x + 65)
+      } else {
+        cipherText += strToUpp[j];
+      }
+    }
+
+    keyToStr = '';
+    if(this.mode === 'false') {
+      return cipherText = cipherText.split('').reverse().join('')
+    } 
+    return cipherText 
+  }
+  decrypt(str, key) {
+    let strToUpp = str.toUpperCase()
+    let keyToStr = this.getStringFromKey(str, key)
+    
+    let originalText = '';
+    for (let j = 0; j < str.length; j++) {
+      if(this.alphabet.includes(str[j])){
+        let x = ((strToUpp.charCodeAt(j) - 65) - (keyToStr.charCodeAt(j) - 65) + 26) % 26;
+        originalText += String.fromCharCode(x + 65)
+      } else {
+        originalText += str[j];
+      }
+    }
+    keyToStr = '';
+    if(this.mode === 'false') {
+      return originalText = originalText.split('').reverse().join('')
+    } 
+    return originalText;
   }
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,34 +11,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(dat) {
+  if (arguments.length == 0) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!date.toJSON()) {
+    throw new Error();
+  }
+  let month = date.getMonth();
+  switch (true) {
+    case month >= 2 && month <= 4: {
+      return 'spring';
+      break;
+    }
+
+    case month >= 5 && month <= 7: {
+      return 'summer';
+      break;
+    }
+
+    case month >= 8 && month <= 10: {
+      return 'autumn';
+      break;
+    }
+
+    default:
+      return 'winter';
+  }
 }
-  // if (arguments.length == 0) {
-  //   return "Unable to determine the time of year!";
-  // }
-  // if (!date.toJSON ()) {
-  //   throw new Error ("Invalid date!");
-  // }
-  // let month = date.getMonth ();
-  // switch (true) {
-  //   case month >= 2 && month <= 4: {
-  //     return 'spring';
-  //     break;
-  //   }
-
-  //   case month >= 5 && month <= 7: {
-  //     return 'summer';
-  //     break;
-  //   }
-
-  //   case month >= 8 && month <= 10: {
-  //     return 'autumn';
-  //     break;
-  //   }
-
-  //   default:
-  //     return 'winter';
-  // }
-
