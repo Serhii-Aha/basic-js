41b9ed451dac0502a801e1dc00443b5e41b2a805
unknown <a1002android@gmail.com>
2021-09-20 21:21:16 +0300

    H:/

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function getCommonCharacterCount(s1, s2) {
+  let pos;
+  let count = 0;
+  let s2arr=s2.split('');
+  for (let i = 0; i < s1.length; i++) {
+    pos = s2.search(s1[i]);
+    if (pos == -1) {
+      continue;
+    } else {
+      s2arr = delete s2arr[pos];
+      count++;
+    }
+      
+  }
+  return count-1;
+  }
+
